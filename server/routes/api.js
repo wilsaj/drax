b50bb85559d52d862b24664ca98757ee93dc36e5
wilsaj <wilson.andrew.j+github@gmail.com>
2014-08-20 17:35:44 -0500

    add clone api method (and drop nodegit in favor of shelling out git cmds)

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,6 +9,9 @@ var util = require('../util');
 // Open the repository in the current directory.
 
 var router = function (config) {
+  var repoPath = config.get('repoPath');
+  var repoUrl = config.get('repoUrl');
+
   var router = express.Router();
 
   // parse application/json and application/x-www-form-urlencoded
@@ -16,9 +19,6 @@ var router = function (config) {
 
   router.route('/branches')
     .get(function(req, res) {
-
-      var repoPath = config.get('repoPath') + '/.git';
-
       util.branches(repoPath)
         .then(function(branches) {
           res.send(JSON.stringify({
@@ -27,6 +27,16 @@ var router = function (config) {
         });
       });
 
+  router.route('/clone')
+    .get(function(req, res) {
+      util.clone(repoUrl, repoPath)
+        .then(function() {
+          res.send(200, 'success');
+        })
+        .catch(function(error) {
+          res.send(403, error.message);
+        });
+      });
   return router;
 };
 
--- server/util.js
+++ server/util.js
@@ -1,30 +1,42 @@
 'use strict';
 
 var Promise = require('bluebird');
-var git = require('nodegit');
+var execAsync = Promise.promisify(require('child_process').exec);
 var _s  = require('underscore.string');
 
-var openRepo = Promise.promisify(git.Repo.open);
-Promise.promisifyAll(git.Repo.prototype);
+
+function git(subcommand, repoPath) {
+  var options = {};
+
+  if (repoPath !== undefined) {
+    options.cwd = repoPath;
+  }
+
+  return execAsync('git ' + subcommand, options)
+    .spread(function(stdout, stderr) {
+      return stdout;
+    });
+}
 
 var util = {
   branches: function branches(repoPath) {
-    return openRepo(repoPath)
-      .then(function(repository) {
-        return repository.getReferencesAsync(git.Reference.Type.Oid)
-          .then(function(referenceNames) {
-            return referenceNames
-              .filter(function(referenceName) {
-                return _s.startsWith(referenceName, "refs/heads");
-              })
-              .map(function(referenceName) {
-                return referenceName.substr(11);
-              });
+    return git('branch -r', repoPath)
+      .then(function(output) {
+        return output
+          .split('\n')
+          .filter(function(line) {
+            return line && !_s.startsWith(line, '  origin/HEAD ->');
+          })
+          .map(function(line) {
+            return line.substr(9);
           });
       })
       .catch(function(error) {
-        console.log("Error: " + error.message);
+        console.log("git branches error: " + error.message);
       });
+  },
+  clone: function clone(url, repoPath) {
+    return git('clone ' + url + ' ' + repoPath);
   }
 };
 
