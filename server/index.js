98c258c5b55e28cd47c0c24f27762e4266c0a8b2
wilsaj <wilson.andrew.j+github@gmail.com>
2014-08-28 16:54:23 -0500

    add socket.io to the mix

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 'use strict';
 var express = require('express');
+var http = require('http');
 
 var Config = require('./config');
 var Routes = require('./routes');
@@ -8,17 +9,21 @@ var Server = function (options) {
   var app = express();
   var config = Config(options);
 
+  var server = require('http').Server(app);
+  var io = require('socket.io')(server);
+
   // set up routes, mapping the .dist/client dir to be served as static content
   var staticDir = './client';
-  app.use('/', Routes(staticDir, config));
+  app.use('/', Routes(staticDir, config, io));
   app.set('views', './server/views');
+
 
   return {
     app: app,
     start: function() {
       var port = config.get('port');
 
-      app.listen(port, function () {
+      server.listen(port, function () {
         console.log('Express server listening on port %d in %s mode', port, app.get('env'));
       });
     }
