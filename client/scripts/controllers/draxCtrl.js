25107591fd7a79d98fd84f3903b985483e383239
wilsaj <wilson.andrew.j+github@gmail.com>
2014-09-02 11:09:29 -0500

    convert to commits obj keyed by commit hash for more efficient message handling

--- client/scripts/controllers/draxCtrl.js
+++ client/scripts/controllers/draxCtrl.js
@@ -1,11 +1,18 @@
 angular.module('draxApp')
-  .controller('draxCtrl', function ($scope, DataService) {
+  .controller('draxCtrl', function ($scope, DataService, SocketService) {
     'use strict';
 
-    $scope.commits = [];
+    $scope.commits = {};
 
     DataService.getCommits()
       .then(function (commits) {
-        $scope.commits = commits;
+        _.forEach(commits, function(commit) {
+          $scope.commits[commit.hash] = commit;
+        });
       });
+
+    SocketService.on('build', function (message) {
+      var commit = $scope.commits[message.commit];
+      commit.status = message.status;
+    });
   });
--- client/scripts/directives/draxCommit.js
+++ client/scripts/directives/draxCommit.js
@@ -1,16 +1,10 @@
 angular.module('draxApp')
-  .directive('draxCommit', function(DataService, SocketService) {
+  .directive('draxCommit', function(DataService) {
     'use strict';
     return {
       restrict: 'A',
       templateUrl: 'commit.html',
       controller: function ($scope) {
-
-        SocketService.on('build', function (message) {
-          if (message.commit === $scope.commit.hash) {
-            $scope.commit.status = message.status;
-          }
-        });
 
         $scope.build = function () {
           DataService.build($scope.commit.hash)
