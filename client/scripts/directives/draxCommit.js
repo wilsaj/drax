541ab2ec9d668a2d6bcb8690549beede9358fbf7
wilsaj <wilson.andrew.j+github@gmail.com>
2014-09-08 14:29:11 -0500

    add deployment labels to commit display

--- client/scripts/directives/draxCommit.js
+++ client/scripts/directives/draxCommit.js
@@ -5,12 +5,18 @@ angular.module('draxApp')
       restrict: 'A',
       templateUrl: 'commit.html',
       controller: function ($scope) {
+        $scope.deployedTo = [];
+
         $scope.build = function () {
           DataService.build($scope.commit.hash)
             .then(function (status) {
               $scope.commit.status = status;
             });
         };
+
+        $scope.$watch('deployments', function (deployments) {
+          $scope.deployedTo = _.filter(deployments, {'commit': $scope.commit.hash});
+        });
       }
     };
   });
